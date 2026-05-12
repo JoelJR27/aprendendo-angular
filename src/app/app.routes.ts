e7738033e941d238bdcdee449341947a8d7f184f
Joel Júnior <devjoeljunior@gmail.com>
2026-05-11 21:32:20 -0300

    finalizar página de estrutura de um projeto angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,18 @@
 import { Routes } from '@angular/router';
 import { Roadmap } from './pages/roadmap/roadmap';
 import { OQueEAngularESuaFilosofia } from './pages/o-que-e-angular-e-sua-filosofia/o-que-e-angular-e-sua-filosofia';
+import { EstruturaDeUmProjetoAngular } from './pages/estrutura-de-um-projeto-angular/estrutura-de-um-projeto-angular';
 
 export const routes: Routes = [
+  {
+    path: "",
+    component: OQueEAngularESuaFilosofia,
+  },
   {
     path: 'roadmap',
     component: Roadmap
   },
-  {
-    path: "o-que-e-angular-e-sua-filosofia",
-    component: OQueEAngularESuaFilosofia,
+  { path: "estrutura-de-um-projeto-angular",
+    component: EstruturaDeUmProjetoAngular
   }
 ];
---
+++ src/app/pages/estrutura-de-um-projeto-angular/estrutura-de-um-projeto-angular.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estrutura-de-um-projeto-angular',
+  imports: [],
+  templateUrl: './estrutura-de-um-projeto-angular.html',
+  styleUrl: './estrutura-de-um-projeto-angular.css',
+})
+export class EstruturaDeUmProjetoAngular {}

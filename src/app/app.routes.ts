ead62680b9cffa8f66a9f58d41ea7b73aacd03e2
Joel NotebookVaio <joelviana250@gmail.com>
2026-05-13 12:33:44 -0300

    finalizar 4. Arquitetura Baseada em Componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Roadmap } from './pages/roadmap/roadmap';
 import { OQueEAngularESuaFilosofia } from './pages/o-que-e-angular-e-sua-filosofia/o-que-e-angular-e-sua-filosofia';
 import { EstruturaDeUmProjetoAngular } from './pages/estrutura-de-um-projeto-angular/estrutura-de-um-projeto-angular';
 import { AngularCli } from './pages/angular-cli/angular-cli';
+import { ArquiteturaBaseadaEmComponentes } from './pages/arquitetura-baseada-em-componentes/arquitetura-baseada-em-componentes';
 
 export const routes: Routes = [
   {
@@ -20,5 +21,9 @@ export const routes: Routes = [
   {
     path: "angular-cli",
     component: AngularCli
+  },
+  {
+    path: "arquitetura-baseada-em-componentes",
+    component: ArquiteturaBaseadaEmComponentes
   }
 ];
---
+++ src/app/pages/arquitetura-baseada-em-componentes/arquitetura-baseada-em-componentes.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-arquitetura-baseada-em-componentes',
+  imports: [],
+  templateUrl: './arquitetura-baseada-em-componentes.html',
+  styleUrl: './arquitetura-baseada-em-componentes.css',
+})
+export class ArquiteturaBaseadaEmComponentes {}
